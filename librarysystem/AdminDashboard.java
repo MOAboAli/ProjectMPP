629b96bdc80b56d7083d6ca513362e6a4a73c02a
MoAboali <mohamed.aboali20@outlook.com>
2024-06-06 14:23:38 -0500

    Merge to Div

--- librarysystem/AdminDashboard.java
+++ librarysystem/AdminDashboard.java
@@ -1,16 +1,20 @@
 package librarysystem;
 
+import utils.Utils;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.HashMap;
 
 public class AdminDashboard extends JFrame {
+    static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
-    public final static int WIDTH = 800;
-    public final static int HEIGHT = 600;
+    public final static int WIDTH = (int) (0.8 * screenSize.width);
+    public final static int HEIGHT = (int) (0.8 * screenSize.height);
     private JButton selectedButton;
     JPanel rightPanel ;
     JPanel rightContainer;
@@ -40,33 +44,35 @@ public class AdminDashboard extends JFrame {
         // Add navigation buttons
         HashMap<String, JPanel> navItems = new HashMap<>();
         navItems.put("Book Check Out", new CheckOutBooksWindow());
-        navItems.put("Item 1", null);
+        navItems.put("New Member", new AddNewMemberWindow());
         navItems.put("Item 2", null);
         navItems.put("Item 3", null);
 
         for (HashMap.Entry<String, JPanel> item : navItems.entrySet()) {
-            JButton button = createCustomButton(item.getKey(), "", new Font("Roboto Mono", Font.PLAIN, 12), e -> {
-                if (selectedButton!=null){
-                    selectedButton.setForeground(Color.BLACK); // Change text color back to normal
-                    selectedButton.setBorderPainted(false); // Hide border
-                    selectedButton.setOpaque(false); // Make background transparent
-                    selectedButton.setBackground(null); // Reset background
+            JButton button = createCustomButton(item.getKey(), "", new Font("Roboto Mono", Font.PLAIN, 12));
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (selectedButton!=null){
+                        selectedButton.setForeground(Color.BLACK); // Change text color back to normal
+                        selectedButton.setBorderPainted(false); // Hide border
+                        selectedButton.setOpaque(false); // Make background transparent
+                        selectedButton.setBackground(null); // Reset background
+                    }
+                    //System.out.println(item + " clicked");
+                    selectedButton = button;
+                    button.setForeground(Color.WHITE); // Change text color on hover
+                    button.setOpaque(true); // Make background opaque
+                    button.setBackground(Color.GRAY);
+                    updateRightPanel( item.getValue());
                 }
-                //System.out.println(item + " clicked");
-                selectedButton = button;
-                button.setForeground(Color.WHITE); // Change text color on hover
-                button.setOpaque(true); // Make background opaque
-                button.setBackground(Color.GRAY);
-                updateRightPanel( item.getValue());
             });
             button.setAlignmentX(Component.LEFT_ALIGNMENT);
             leftPanel.add(button);
         }
 
         // Add settings icon
-        JButton settingsLabel = createCustomButton("Logout", "C:\\Users\\salah\\OneDrive\\Desktop\\logout.png", new Font("Roboto Mono", Font.PLAIN, 12), e -> {
-            System.out.println("Logout clicked");
-        });
+        JButton settingsLabel = createCustomButton("Logout", Utils.assets_dir+"logout.png", new Font("Roboto Mono", Font.PLAIN, 12));
         settingsLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         leftPanel.add(Box.createVerticalGlue());
@@ -79,27 +85,12 @@ public class AdminDashboard extends JFrame {
         //rightPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
         // Add form fields
-        String[] labels = {"Tag ID", "Neptun", "Név", "Könyvek száma", "Email", "Összes díj"};
-        JTextField[] textFields = new JTextField[labels.length];
 
-        for (int i = 0; i < labels.length; i++) {
-            rightPanel.add(new JLabel(labels[i]));
-            textFields[i] = new JTextField();
-            rightPanel.add(textFields[i]);
-        }
-
-        // Add action buttons
-        JButton deleteButton = new JButton("Tag törlése");
-
-
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
-        buttonPanel.add(deleteButton);
 
 
         // Add right panel and button panel into a container
         rightContainer = new JPanel(new BorderLayout());
-        rightContainer.add(new AddNewMemberWindow(), BorderLayout.BEFORE_FIRST_LINE);
+        rightContainer.add(new JPanel(), BorderLayout.BEFORE_FIRST_LINE);
         // Split pane
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, rightContainer);
         splitPane.setDividerSize(0);
@@ -111,7 +102,7 @@ public class AdminDashboard extends JFrame {
         setVisible(true);
     }
 
-    private JButton createCustomButton(String text, String iconPath, Font font, ActionListener listener) {
+    private JButton createCustomButton(String text, String iconPath, Font font) {
         JButton button = new JButton(text);
         button.setFont(font); // Set custom font
         button.setContentAreaFilled(false); // Remove button background
